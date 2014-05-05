2b5488f24df04ac8b7e7836d5933ac9ca35d8e2e
Justen Doherty <justen@wedo.co.uk>
2014-05-05 18:26:11 +0100

    Added homepage

--- laravel/app/views/home.blade.php
+++ laravel/app/views/home.blade.php
@@ -24,10 +24,68 @@ Diamonds, Gold, German Property, Water Rights, Investment
 @include('includes.contactForm')
 @stop
 
+<?php
+$diamondUrl = action('HomeController@investInDiamonds');
+$goldUrl    = action('HomeController@investInGold');
+$germanUrl  = action('HomeController@investInGermanProperty');
+$waterUrl   = action('HomeController@investInWaterRights');
+?>
+
+
 @section('intro')
-<div class="span12">
-    <hgroup id="intro" style="display: none">
-        <h2>London DE</h2>
-    </hgroup>
-</div>
+<section id="home" class="box">
+    <div class="container">
+        <div class="panel">
+            <div class="row-fluid">
+                <div class="span12 header">
+                    <hgroup>
+                        <h2>Welcome to London DE LTD</h2>
+                    </hgroup>
+                </div>
+            </div>
+            <div class="row-fluid">
+                <div class="span12 content">
+                    <div class="row-fluid">
+                        <div class="span2">
+                            <a href="<?php echo $diamondUrl; ?>"><img class="img-polaroid"
+                                 src="http://placehold.it/350x350"/></a>
+                        </div>
+                        <div class="span4">
+                            <h3><a href="<?php echo $diamondUrl; ?>">Invest in Diamonds</a></h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu nisi sit amet sem rutrum scelerisque.</p>
+                        </div>
+                        <div class="span2">
+                            <a href="<?php echo $goldUrl; ?>"><img class="img-polaroid"
+                                 src="http://placehold.it/350x350"/></a>
+                        </div>
+                        <div class="span4">
+                            <h3><a href="<?php echo $goldUrl; ?>">Invest in Gold</a></h3>
+                            <p>In scelerisque convallis nisi non feugiat. In tempor magna id viverra pharetra. Nulla eu lectus sit amet sem fringilla commodo eu molestie velit</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row-fluid">
+                        <div class="span2">
+                            <a href="<?php echo $germanUrl; ?>"><img class="img-polaroid"
+                                 src="http://placehold.it/350x350"/></a>
+                        </div>
+                        <div class="span4">
+                            <h3><a href="<?php echo $germanUrl; ?>">Invest in German Property</a></h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu nisi sit amet sem rutrum scelerisque.</p>
+                        </div>
+                        <div class="span2">
+                            <a href="<?php echo $waterUrl; ?>"><img class="img-polaroid"
+                                 src="http://placehold.it/350x350"/></a>
+                        </div>
+                        <div class="span4">
+                            <h3><a href="<?php echo $waterUrl; ?>">Invest in Water Rights</a></h3>
+                            <p>In scelerisque convallis nisi non feugiat. In tempor magna id viverra pharetra. Nulla eu lectus sit amet sem fringilla commodo eu molestie velit</p>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @stop
